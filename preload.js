c94e4177c805cf4ec961d254bc82c0ce6aa98d17
Phasit Wosri <phasit@Phasits-MacBook-Air.local>
2023-12-18 20:37:48 +0700

    essesntialPath

--- preload.js
+++ preload.js
@@ -1,10 +1,34 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const replaceText = (selector, text) => {
-      const element = document.getElementById(selector)
-      if (element) element.innerText = text
-    }
-  
-    for (const dependency of ['chrome', 'node', 'electron']) {
-      replaceText(`${dependency}-version`, process.versions[dependency])
-    }
-  })
+const { contextBridge, ipcRenderer } = require('electron')
+var fs = require('fs')
+
+// check cache data
+var essentialPath
+const filename = "settings.json"
+fs.exists(filename, (exists) => {
+  if (!exists) {
+    // Create file
+    let obj = { school: null, invoiceForm1: null, invoiceForm2: null, invoiceForm3: null, invoiceForm4: null }
+    let json = JSON.stringify(obj)
+    fs.writeFile(filename, json, (err) => console.log(err))
+  }
+  else {
+    console.log("settings.json exists");
+    fs.readFile(filename, (err, data) => {
+      if (err)
+        console.log(err)
+      else {
+        essentialPath = data
+        console.log(data)
+      }
+    })
+  }
+});
+
+function loadDB() {
+
+}
+
+// global var
+contextBridge.exposeInMainWorld('preload', {
+  schoolData: loadDB()
+})
