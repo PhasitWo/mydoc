2430404cec7d0149ba765f4a1cb255596087cb0b
Phasit Wosri <phasit@Phasits-MacBook-Air.local>
2023-12-21 01:45:09 +0700

    load address

--- renderer.js
+++ renderer.js
@@ -1,14 +1,28 @@
 // navbar
 $("#link1").click(function () {
     $("#content").load("setting.html");
+    document.getElementById("active").style.display = "block"
+    document.getElementById("active").style.top = "0%"
+    document.getElementById("link1").classList.add("black")
+    document.getElementById("link2").classList.remove("black")
+    document.getElementById("link3").classList.remove("black")
 });
 
 $("#link2").click(function () {
     $("#content").load("procurement.html");
+    document.getElementById("active").style.display = "block"
+    document.getElementById("active").style.top = "35.33%"
+    document.getElementById("link1").classList.remove("black")
+    document.getElementById("link2").classList.add("black")
+    document.getElementById("link3").classList.remove("black")
 });
-
 $("#link3").click(function () {
     $("#content").load("receipt.html");
+    document.getElementById("active").style.display = "block"
+    document.getElementById("active").style.top = "68.66%"
+    document.getElementById("link1").classList.remove("black")
+    document.getElementById("link2").classList.remove("black")
+    document.getElementById("link3").classList.add("black")
 });
 // load database
 var database
@@ -18,7 +32,7 @@ api.loadDB().then((result) => {
     else if (result == null) {
         alert("please set path to database in Setting")
         $("#link1").click()
-    }   
+    }
     database = result
 })
 
