ef760c621e262da9f2ba692978245d42aacfcfe8
PhasitWo <vrzoearth@gmail.com>
2024-01-09 14:22:02 +0700

    1.0.5 refactor index.js, make switching between page smoother

--- js/index.js
+++ js/index.js
@@ -1,37 +1,55 @@
 // navbar
-$("#link1").click(function () {
-    $("#content-style").attr("href", "../css/setting.css");
-    $("#content").load("setting.html");
-    document.getElementById("active").style.display = "block";
-    document.getElementById("active").style.top = "2%";
-    document.getElementById("link1").classList.add("black");
-    document.getElementById("link2").classList.remove("black");
-    document.getElementById("link3").classList.remove("black");
-});
-$("#link2").click(function () {
-    $("#content-style").attr("href", "../css/procurement.css")
-    $("#content").load("procurement.html");
-    document.getElementById("active").style.display = "block";
-    document.getElementById("active").style.top = "35.33%";
-    document.getElementById("link1").classList.remove("black");
-    document.getElementById("link2").classList.add("black");
-    document.getElementById("link3").classList.remove("black");
-});
-$("#link3").click(function () {
-    $("#content-style").attr("href", "../css/receipt.css");
-    $("#content").load("receipt.html");
-    document.getElementById("active").style.display = "block";
-    document.getElementById("active").style.top = "68.66%";
-    document.getElementById("link1").classList.remove("black");
-    document.getElementById("link2").classList.remove("black");
-    document.getElementById("link3").classList.add("black");
-});
+let anchors = document.querySelectorAll(".navbar-link a");
+for (let i = 0; i < anchors.length; i++) {
+    let a = anchors[i]
+    a.onclick = () => {
+        // change page
+        $("#content-style").attr("href", `../css/${a.id}.css`);
+        $("#content").css("display", "none");
+        $("#content").load(`${a.id}.html`, () => $("#content").fadeIn("fast"));
+        // 'active' and 'passive' div        
+        anchors.forEach((a) => {
+            a.classList.remove("black");
+        });
+        a.classList.add("black");
+        document.getElementById("active").style.display = "block";
+        document.getElementById("active").style.top = (i * 33.33 + 2) + "%";
+    };
+}
+
+// $("#link1").click(function () {
+//     $("#content-style").attr("href", "../css/setting.css");
+//     $("#content").load("setting.html");
+//     document.getElementById("active").style.display = "block";
+//     document.getElementById("active").style.top = "2%";
+//     document.getElementById("link1").classList.add("black");
+//     document.getElementById("link2").classList.remove("black");
+//     document.getElementById("link3").classList.remove("black");
+// });
+// $("#link2").click(function () {
+//     $("#content-style").attr("href", "../css/procurement.css");
+//     $("#content").load("procurement.html");
+//     document.getElementById("active").style.display = "block";
+//     document.getElementById("active").style.top = "35.33%";
+//     document.getElementById("link1").classList.remove("black");
+//     document.getElementById("link2").classList.add("black");
+//     document.getElementById("link3").classList.remove("black");
+// });
+// $("#link3").click(function () {
+//     $("#content-style").attr("href", "../css/receipt.css");
+//     $("#content").load("receipt.html");
+//     document.getElementById("active").style.display = "block";
+//     document.getElementById("active").style.top = "68.66%";
+//     document.getElementById("link1").classList.remove("black");
+//     document.getElementById("link2").classList.remove("black");
+//     document.getElementById("link3").classList.add("black");
+// });
 $("#welcome").click(function () {
     $("#content").load("welcome.html");
-    document.getElementById("active").style.display = "none"
-    document.getElementById("link1").classList.remove("black");
-    document.getElementById("link2").classList.remove("black");
-    document.getElementById("link3").classList.remove("black");
+    document.getElementById("active").style.display = "none";
+    document.querySelectorAll(".navbar-link a").forEach((a) => {
+        a.classList.remove("black");
+    });
 });
 
 // load database
